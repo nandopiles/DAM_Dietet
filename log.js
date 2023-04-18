9753e74ee249335de1f28eab02a825e0366d6fa7
Nando PL <nandopiless@gmail.com>
2023-04-18 21:07:20 +0200

    first touch main_window with the template

--- log.js
+++ log.js
@@ -14,7 +14,7 @@ async function run() {
         const usersCollection = database.collection('users');
         //paso toda la info de la Colección Users a un Array para trabajar con él
         users = await usersCollection.find({}).toArray()
-        //users.forEach(user => console.log(user))
+        users.forEach(user => console.log(user))
     } finally {
         //Cliente se cerrará cuando la aplicación finalice/error
         await client.close();
@@ -23,7 +23,6 @@ async function run() {
 
 run()
     .then(() => {
-        let userSelected
         let userFound = false
 
         document.getElementById("btnLogin").addEventListener('click', (e) => {
@@ -33,9 +32,8 @@ run()
                 if (users[i].name == document.getElementById("username").value &&
                     users[i].password == document.getElementById("password").value) {
                     console.log("[+] User validated => " + users[i].name)
-                    userSelected = users[i]
                     userFound = true
-                    window.location = "main-window.html" //redireccionar a la vista utilizada (html)
+                    window.location = "main_window.html?name=" + users[i].name
                     return;
                 }
             }
